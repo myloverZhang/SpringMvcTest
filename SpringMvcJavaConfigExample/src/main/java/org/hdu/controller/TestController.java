3b95b48569f8e76833f108519fbb084eacbc65e0
shushoufu <zhangzhengdi@foxmail.com>
2017-12-20 16:26:08 +0800

    。。。

--- SpringMvcJavaConfigExample/src/main/java/org/hdu/controller/TestController.java
+++ SpringMvcJavaConfigExample/src/main/java/org/hdu/controller/TestController.java
@@ -1,7 +1,13 @@
 package org.hdu.controller;
 
+import com.sun.org.apache.bcel.internal.generic.RET;
+import org.hdu.model.SchoolPo;
+import org.hdu.model.StudentsPo;
+import org.hdu.service.ITestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,8 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 public class TestController {
+    @Autowired
+    private ITestService testService;
+
     @GetMapping(value = "/test")
     public String test(){
         return "hello world";
+    }
+
+    @GetMapping(value = "/studentById")
+    public StudentsPo getStudentById(@RequestParam Integer id){
+        return testService.getStudentInfo(id);
+    }
+
+    @GetMapping(value = "/schoolById")
+    public SchoolPo getSchoolById(@RequestParam Integer id){
+        return  testService.getSchoolInfo(id);
     }
 }
